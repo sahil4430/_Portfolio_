7be0d1478acabfd90ea679b8bed7faca4cc3dbef
sahil4430 <panwarsahil204@gmail.com>
2024-10-14 19:50:00 +0530

    here we go again

---
+++ src/Components/Index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { TabsDemo } from './ui/TabDemo'
+import { FloatingDockDemo } from './ui/NavBar'
+
+function Index() {
+  return (
+    <div>
+      <TabsDemo/>
+      {/* <br/><FloatingDockDemo/> */}
+    </div>
+  )
+}
+
+export default Index
