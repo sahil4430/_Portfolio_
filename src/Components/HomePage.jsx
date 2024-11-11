96adf2193bd0ab4158bc797702e4a02b3b4c3fb5
sahil4430 <panwarsahil204@gmail.com>
2024-11-11 19:47:21 +0530

    let's recreate the portfolio using some ui library

---
+++ src/Components/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { HeroScrollDemo } from './Hero'
+import { TabsDemo } from './Project'
+import { FloatingDockDemo} from './FloatingDockDemo'
+
+function HomePage() {
+  return (
+    <div>
+     <FloatingDockDemo/>
+      <HeroScrollDemo/><br/>
+      <TabsDemo/><br/>
+    </div>
+  )
+}
+export default HomePage
